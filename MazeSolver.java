1040a9cf749aeaebe1c81ae38e726904432871be
En Yu Zhang <Flashlight@Ens-MacBook-Pro.local>
2018-04-12 20:58:59 -0400

    fixed up formatting

--- MazeSolver.java
+++ MazeSolver.java
@@ -9,24 +9,22 @@ private static int[] directions = {Maze.EAST, Maze.NORTH, Maze.SOUTH, Maze.WEST}
   public static boolean solveThis(Maze maze) {
 
     if (maze.explorerIsOnA() == Maze.TREASURE) { //base cases
-          return true;
+      return true;
       }
     else if (maze.explorerIsOnA() == Maze.WALL) {
       return false;
     }
     else {
-        Maze snapshot = new Maze(maze); //using the Maze(old maze) constructor
-        maze.dropA(Maze.WALL);
-        for (int eachDirection : directions) {
-      maze.go(eachDirection);
-      if (solveThis(maze)) {
-                 return true;
-             }
-             else {
-                 maze = snapshot;
-             }
+      Maze snapshot = new Maze(maze); //using the Maze(old maze) constructor
+      maze.dropA(Maze.WALL);
+      for (int eachDirection : directions) {
+        maze.go(eachDirection);
+        if (solveThis(maze)) {
+          return true; }
+        else {
+          maze = snapshot;}
         }
       }
-      return false; 
+    return false; 
   }
 }
