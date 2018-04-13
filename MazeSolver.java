6635e687f93b6140116cd0950560b300181ce59b
En Yu Zhang <Flashlight@Ens-MacBook-Pro.local>
2018-04-12 20:50:20 -0400

    solveThis method

--- MazeSolver.java
+++ MazeSolver.java
@@ -4,16 +4,29 @@ public class MazeSolver {
 
  See README for specifics. (statement of the problem, etc)
 */
+private static int[] directions = {Maze.EAST, Maze.NORTH, Maze.SOUTH, Maze.WEST};
 
   public static boolean solveThis(Maze maze) {
-      if (maze.explorerIsOnA() == Maze.TREASURE) { //base cases
+
+    if (maze.explorerIsOnA() == Maze.TREASURE) { //base cases
           return true;
       }
     else if (maze.explorerIsOnA() == Maze.WALL) {
       return false;
-    } 
-    else { //explorer must be on a stepping stone, recursive case
-    return false;
     }
+    else {
+        Maze snapshot = new Maze(maze); //using the Maze(old maze) constructor
+        maze.dropA(Maze.WALL);
+        for (int eachDirection : directions) {
+      maze.go(eachDirection);
+      if (solveThis(maze)) {
+                 return true;
+             }
+             else {
+                 maze = snapshot;
+             }
+        }
+      }
+      return false; 
   }
 }
